18f7187622061a0845fcdc971b4de42ccf8de695
mauri0098 <maurirodriguez0098@gmail.com>
2023-05-19 14:20:20 -0300

    19/05/23

--- pryGestion/frmMain.Designer.cs
+++ pryGestion/frmMain.Designer.cs
@@ -34,7 +34,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 36);
+            this.label1.Location = new System.Drawing.Point(53, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(418, 13);
             this.label1.TabIndex = 1;
