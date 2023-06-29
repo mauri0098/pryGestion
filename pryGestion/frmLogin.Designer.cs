8b065c98bb47e7fa9eca4b66c157be90a4171d08
mauri0098 <maurirodriguez0098@gmail.com>
2023-06-29 01:26:22 -0300

    28/06/2023

--- pryGestion/frmLogin.Designer.cs
+++ pryGestion/frmLogin.Designer.cs
@@ -91,7 +91,7 @@
             // 
             this.lblUsuario.AutoSize = true;
             this.lblUsuario.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUsuario.Location = new System.Drawing.Point(26, 31);
+            this.lblUsuario.Location = new System.Drawing.Point(26, 36);
             this.lblUsuario.Name = "lblUsuario";
             this.lblUsuario.Size = new System.Drawing.Size(45, 14);
             this.lblUsuario.TabIndex = 7;
